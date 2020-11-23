f997f8c8a28750517028c9c031a7fa02518a8296
karanhappysanz <karan.happysanz@gmail.com>
2020-11-23 16:53:08 +0530

    test2

--- Sample/AppDelegate.swift
+++ Sample/AppDelegate.swift
@@ -14,7 +14,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-        //Check
+        //Check project Sample
         return true
     }
 
